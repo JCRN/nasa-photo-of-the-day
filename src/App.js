62d52bfbe2ed2f303c7d293b567af7fec44088e5
JCRN <clemons.jason@gmail.com>
2019-07-17 19:35:45 -0700

    WIP: building components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
-import React from "react";
-import "./App.css";
+import axios from "axios";
+import React, { useState, useEffect } from "react";
+import "./index.scss";
 
 function App() {
+  const [media, setMedia] = useState("");
+  useEffect(() => {
+    axios
+      .get(
+        `https://api.nasa.gov/planetary/apod?api_key=PcVpaedF6F77PzIsMXSmtdGVbICPmYJVGGez97Xg`
+      )
+      .then(result => {
+        console.log("result:", result);
+        setMedia(result.data);
+      })
+      .catch(error => console.log("ERROR: ", error));
+  }, []);
+  console.log("media line 29: ", media);
   return (
     <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun 🚀!
-      </p>
+      {console.log(media)}
+      <p>Test This Mess</p>
     </div>
   );
 }
